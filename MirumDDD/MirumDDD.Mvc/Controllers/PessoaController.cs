b8ba7d4f633a27e32636d3aa0c60c303792d706e
Miguel dos Martyres <fullmdm@outlook.com>
2019-05-12 20:29:11 -0300

    Primeiras telas e nuget do entity core

---
+++ MirumDDD/MirumDDD.Mvc/Controllers/PessoaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MirumDDD.Mvc.Controllers
+{
+    public class PessoaController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Listar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Cadastrar()
+        {
+            return View();
+        }
+    }
+}
